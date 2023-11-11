8c639ca9204f04256ce35d0a2a65e7588482b92b
AndreDu95 <2220894@my.ipleiria.pt>
2023-11-11 16:04:31 +0000

    Criado apenas o fragmento da lista de favoritos
    Nao esta mais nada implementado, pois nao tenho a certeza se devia criar o layout ou aproveitamos um dos artigos que ja temos visto que temos dois layouts dos artigos. Tambem fiquei na duvida de criar ou não a classe dos favoritos porque os items dos favoritos irão ser os artigos

---
+++ BrindesZorro/app/src/main/java/pt/ipleiria/estg/dei/brindeszorro/fragment/ListaFavoritosFragment.java
@@ -0,0 +1,25 @@
+package pt.ipleiria.estg.dei.brindeszorro.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pt.ipleiria.estg.dei.brindeszorro.R;
+
+public class ListaFavoritosFragment extends Fragment {
+
+    public ListaFavoritosFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lista_favoritos, container, false);
+    }
+}
