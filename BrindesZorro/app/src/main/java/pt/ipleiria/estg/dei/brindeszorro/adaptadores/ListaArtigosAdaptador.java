e4954b9905ad45c2675a7c7bd9a3b570347dc860
AndreDu95 <2220894@my.ipleiria.pt>
2023-11-11 15:30:58 +0000

    Continuidade do desenvolvimento do adaptador da lista de artigos
    ainda não está em funcionamento, devido aos campos/atributos dos id's do layout dos artigos

--- BrindesZorro/app/src/main/java/pt/ipleiria/estg/dei/brindeszorro/adaptadores/ListaArtigosAdaptador.java
+++ BrindesZorro/app/src/main/java/pt/ipleiria/estg/dei/brindeszorro/adaptadores/ListaArtigosAdaptador.java
@@ -5,10 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import pt.ipleiria.estg.dei.brindeszorro.R;
 import pt.ipleiria.estg.dei.brindeszorro.modelo.Artigo;
+import pt.ipleiria.estg.dei.brindeszorro.modelo.Fatura;
 
 public class ListaArtigosAdaptador extends BaseAdapter {
 
@@ -42,7 +45,42 @@ public class ListaArtigosAdaptador extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+    public View getView(int position, View convertView, ViewGroup viewGroup) {
+        if (inflater == null)
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE); // autorização a variacvel
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.activity_artigos, null);
+        }
+        ViewHolderLista viewHolderLista = (ListaArtigosAdaptador.ViewHolderLista) convertView.getTag();
+        if (viewHolderLista ==null){
+            viewHolderLista = new ListaArtigosAdaptador.ViewHolderLista(convertView);
+            convertView.setTag(viewHolderLista);
+        }
+
+        viewHolderLista.update(artigos.get(position));
+        return convertView;
     }
+
+    private class ViewHolderLista {
+        private TextView tvId, tvPreco, tvNome, tvDescricao, tvReferencia;
+
+        // ELABORAR BEM ESTE METODO CONSOANTE O LAYOUT DO ARTIGO QUE FOR PARA USAR!!!!!
+        // ESTA COMENTADO POIS NEM TODOS OS ID's DE ATRIBUTOS ESTÃO BEM DEFINIDOS NO LAYOUT
+        public ViewHolderLista(View view) {
+            //tvId = view.findViewById(R.id.tvIdArtigo);
+            //tvPreco = view.findViewById(R.id.tvPrecoArtigo);
+            //tvNome = view.findViewById(R.id.tvNomeArtigo);
+            //tvDescricao = view.findViewById(R.id.tvDescricao);
+            //tvReferencia = view.findViewById(R.id.tvReferenciaArtigo);
+        }
+
+        public void update(Artigo artigo) {
+            tvId.setText(""+artigo.getId());
+            tvPreco.setText(""+artigo.getPreco());
+            tvNome.setText(""+artigo.getNome());
+            tvDescricao.setText(""+artigo.getNome());
+            tvReferencia.setText(""+artigo.getReferencia());
+        }
+    }
+
 }
