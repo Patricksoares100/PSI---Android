abdcf2cb05187b434995f58b459319e12d796c68
Patricksoares100 <2220905@my.ipleiria.pt>
2023-11-18 17:49:06 +0000

    alguma limpeza e formatação de menu
    Ainda não está a pucar a nav para o home, apenas para o menu main

--- BrindesZorro/app/src/main/java/pt/ipleiria/estg/dei/brindeszorro/HomeActivity.java
+++ BrindesZorro/app/src/main/java/pt/ipleiria/estg/dei/brindeszorro/HomeActivity.java
@@ -1,14 +1,26 @@
 package pt.ipleiria.estg.dei.brindeszorro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class HomeActivity extends AppCompatActivity {
 
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolBar); // Configura a barra de ferramentas (toolbar)
+        setSupportActionBar(toolbar); // Define a barra de ferramentas como a barra de apoio da atividade
+        drawer = findViewById(R.id.drawerLayout); // Obtém a gaveta (drawer)
+        navigationView = findViewById(R.id.navView); // Obtém a vista de navegação (navigation view)
+
     }
 }
--- BrindesZorro/app/src/main/java/pt/ipleiria/estg/dei/brindeszorro/fragment/ListaArtigosFragment.java
+++ BrindesZorro/app/src/main/java/pt/ipleiria/estg/dei/brindeszorro/fragment/ListaArtigosFragment.java
@@ -29,9 +29,9 @@ public class ListaArtigosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_lista_artigos, container, false);
+        View view = inflater.inflate(R.layout.activity_home, container, false);
 
-        lvArtigos = view.findViewById(R.id.lvArtigos);
+        lvArtigos = view.findViewById(R.id.lvHome);
         artigos = SingletonGestorLoja.getInstance().getArtigos();
 
         System.out.println("--->" + artigos);
