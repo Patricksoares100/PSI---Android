0357f581ec18072f12273ccbfff279bfc75068bc
AndreDu95 <2220894@my.ipleiria.pt>
2023-11-19 21:03:02 +0000

    Carrinho activity a passar para a confirmar compra activity
    OnClick do botão confirmar venda da activity carrinho ja a funcionar e a passar para a activity confirmar compra. foi so para verificar se funcionava bem

--- BrindesZorro/app/src/main/java/pt/ipleiria/estg/dei/brindeszorro/CarrinhoActivity.java
+++ BrindesZorro/app/src/main/java/pt/ipleiria/estg/dei/brindeszorro/CarrinhoActivity.java
@@ -2,7 +2,9 @@ package pt.ipleiria.estg.dei.brindeszorro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CarrinhoActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class CarrinhoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carrinho);
     }
+
+    public void onClickConfirmarCompra(View view){
+        Intent intent = new Intent(this, ConfirmarActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
 }
