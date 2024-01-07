aaabe81398301fe4675bc14dd213188c45ce3d41
RafaelMCoelho92 <114529694+RafaelMCoelho92@users.noreply.github.com>
2024-01-07 12:32:26 +0000

    correção

--- BrindesZorro/app/src/main/java/pt/ipleiria/estg/dei/brindeszorro/modelo/SingletonGestorLoja.java
+++ BrindesZorro/app/src/main/java/pt/ipleiria/estg/dei/brindeszorro/modelo/SingletonGestorLoja.java
@@ -360,7 +360,6 @@ public class SingletonGestorLoja {
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(context, error.getMessage(), Toast.LENGTH_SHORT).show();
                     System.out.println("----> response ERRO ARTIGOS API" + error);
                 }
             });
