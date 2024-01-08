545b93aa66dcb6db6619d496de7b773088f4db4c
RafaelMCoelho92 <114529694+RafaelMCoelho92@users.noreply.github.com>
2024-01-08 12:27:25 +0000

    login, token fica guardado logo nas shared preferences e agora
    os pedidos a API vao ser gets com o token

---
+++ BrindesZorro/app/src/main/java/pt/ipleiria/estg/dei/brindeszorro/utils/Public.java
@@ -0,0 +1,6 @@
+package pt.ipleiria.estg.dei.brindeszorro.utils;
+
+public class Public {
+    public static final String TOKEN = "TOKEN";
+    public static final String DADOS_USER ="DADOS_USER";
+}
