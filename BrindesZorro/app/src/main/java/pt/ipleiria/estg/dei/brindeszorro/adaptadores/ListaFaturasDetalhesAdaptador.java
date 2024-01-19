43829d4db8c94fa9f34b04234b0855f6cde99fbd
Patricksoares100 <2220905@my.ipleiria.pt>
2024-01-19 11:49:41 +0000

    Btn - Avaliar Artigo
    Apenas a abrir e a passar id

--- BrindesZorro/app/src/main/java/pt/ipleiria/estg/dei/brindeszorro/adaptadores/ListaFaturasDetalhesAdaptador.java
+++ BrindesZorro/app/src/main/java/pt/ipleiria/estg/dei/brindeszorro/adaptadores/ListaFaturasDetalhesAdaptador.java
@@ -3,21 +3,31 @@ package pt.ipleiria.estg.dei.brindeszorro.adaptadores;
 import static java.security.AccessController.getContext;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.fragment.app.FragmentManager;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import java.util.ArrayList;
 
+import pt.ipleiria.estg.dei.brindeszorro.AvaliacaoComentarioActivity;
+import pt.ipleiria.estg.dei.brindeszorro.DetalhesArtigoActivity;
+import pt.ipleiria.estg.dei.brindeszorro.EditarDadosPessoaisActivity;
 import pt.ipleiria.estg.dei.brindeszorro.R;
+import pt.ipleiria.estg.dei.brindeszorro.fragment.ListaFaturaDetalhesFragment;
 import pt.ipleiria.estg.dei.brindeszorro.modelo.Artigo;
+import pt.ipleiria.estg.dei.brindeszorro.modelo.Avaliacao;
 import pt.ipleiria.estg.dei.brindeszorro.modelo.Empresa;
 import pt.ipleiria.estg.dei.brindeszorro.modelo.Fatura;
 import pt.ipleiria.estg.dei.brindeszorro.modelo.LinhaFatura;
@@ -31,12 +41,13 @@ public class ListaFaturasDetalhesAdaptador extends BaseAdapter {
     private LayoutInflater inflater;
     private ArrayList<Artigo> artigos;
     private ArrayList<Fatura> faturas;
+    private ArrayList<Avaliacao> avaliacaos;
     private ArrayList<LinhaFatura> linhaFaturas;
 
     public ListaFaturasDetalhesAdaptador(Context context, ArrayList<LinhaFatura> linhaFaturas) {
         this.context = context;
         this.linhaFaturas = linhaFaturas;
-        //this.inflater = LayoutInflater.from(context);
+
     }
 
     @Override
@@ -71,6 +82,15 @@ public class ListaFaturasDetalhesAdaptador extends BaseAdapter {
         }
 
         viewHolderLista.update(linhaFaturas.get(position));
+
+        Button buttonAvaliarArtigo = convertView.findViewById(R.id.buttonAvaliarArtigoDetalhesFatura);
+        buttonAvaliarArtigo.setOnClickListener(v -> {
+            int idArtigo = linhaFaturas.get(position).getId();
+            Intent intent = new Intent(context, AvaliacaoComentarioActivity.class);
+            intent.putExtra(DetalhesArtigoActivity.ID_ARTIGO,(int) idArtigo);
+            System.out.println("---" + idArtigo);
+            context.startActivity(intent);
+        });
 
         return convertView;
     }
